96291ee963fb3a929102c9faa1168514942d8298
Barthelemy Dagenais <bart@cs.mcgill.ca>
2011-02-19 12:39:13 -0500

    added setup.py and others

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+'''
+  Description of the module here.
+
+  :copyright: Copyright 2011 Barthelemy Dagenais
+  :license: BSD, see LICENSE for details
+'''
+
+from distutils.core import setup
+
+setup(name='ghmiles',
+      version='0.1',
+      description='GitHub Milestones',
+      author='Barthelemy Dagenais',
+      author_email='barthe@users.sourceforge.net',
+      license='BSD License',
+      url='https://github.com/bartdag/ghmiles',
+      py_modules=['ghmiles']
+     ) 
